e8c7d970827707a36ccab4eceba54ec1d5035b26
Egor Kovalev <egor.tharf@gmail.com>
2022-05-13 13:14:15 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from kivy.config import Config
+
+Config.set('graphics', 'width', 720) 
+Config.set('graphics', 'height', 1280)
+
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.core.window import Window
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+
+font = './font/Nunito-VariableFont_wght.ttf'
+
+Window.clearcolor = (255, 255, 255, 1)
+
+
+class MyWidget(FloatLayout):
+    pass
+class MyApp(App):
+    def build(self):
+        return MyWidget()
+
+        
+
+
+if __name__ == '__main__':
+    MyApp().run()
