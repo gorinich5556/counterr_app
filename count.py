78a6af4b0a5efa257b47d9b234d262644e7a83ee
Egor Kovalev <egor.tharf@gmail.com>
2022-05-14 18:14:16 +0300

    minus method added

--- count.py
+++ count.py
@@ -5,28 +5,21 @@ class Counter:
     default = 0
     def per(self, text):
         print(text)
+    
     def plus(self, single_number = 1):
         self.default = self.default + single_number
-        print(self.default)
         return self.default
+    
     def reset(self):
         self.default = 0
-        print(self.default)
+        return self.default
+
+    def minus(self, single_number=1):
+        self.default = self.default - single_number
+        return self.default
 
 
-item = Counter()
 
-item.per('Hello!')
-
-
-num = Counter()
-num.plus(3)
-
-num.plus(5)
-
-num.reset()
-
-num.plus(2)
 
 
 
