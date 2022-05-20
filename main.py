3e7b9ebd398caa3ad5de711fcfba2ab1772560a7
Egor Kovalev <egor.tharf@gmail.com>
2022-05-20 20:49:11 +0300

    switch to KivyMD

--- main.py
+++ main.py
@@ -1,42 +1,10 @@
-from tkinter import Widget
-from kivy.config import Config
-from kivy.uix.screenmanager import NoTransition
-
-Config.set('graphics', 'width', 720) 
-Config.set('graphics', 'height', 1280)
-
-from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager, Screen
-
-font = './font/Nunito-VariableFont_wght.ttf'
-
-Window.clearcolor = (255, 255, 255, 1)
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
 
 
-class HelloScreen(Screen):
-    pass
-
-class WorkScreen(Screen):
-    pass
-
-class CanvasWidget(Widget):
-    pass
-
-class MyApp(App):
+class MainApp(MDApp):
     def build(self):
-        sm = ScreenManager()
-        sm = ScreenManager(transition=NoTransition())
-        sm.add_widget(HelloScreen(name='hello'))
-        sm.add_widget(WorkScreen(name='work'))
-
-        return sm
-
-ghfgh = 11
+        return MDLabel(text="Hello, World", halign="center")
 
 
-
-
-if __name__ == '__main__':
-    MyApp().run()
+MainApp().run()
